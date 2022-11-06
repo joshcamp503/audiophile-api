95c7bbabdfb31e199e419a21ffef9f2ad59f9d12
joshcamp503 <joshcamp503@gmail.com>
2022-11-06 03:03:55 -0800

    Fixed bug: updated mongodb connections and got server working for dev

--- server/server.js
+++ server/server.js
@@ -5,9 +5,7 @@ const Product = require("./models/product")
 
 const app = express();
 
-app.use(cors({
-  origin: ['https://joshcamp.dev/audiophile', 'localhost:3000/audiophile']
-}));
+app.use(cors());
 app.use(express.json());
 // app.use(Router)
 
@@ -16,7 +14,7 @@ const username = `joshcamp503`
 const password = `mongodbpassword`
 
 // connect to mongodb
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 const dbURI = `mongodb+srv://joshcamp503:mongodbpassword@cluster0.hogdo.mongodb.net/audiophile?retryWrites=true&w=majority`
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(PORT, () => console.log('connected')))
